c8899a66da659d7850cf91e8ecce87c545e13e4f
A1d3n10 <41d3n10@gmail.com>
2022-07-08 15:46:26 -0300

    leetcode 8

--- leetCode/medium/Solution.java
+++ leetCode/medium/Solution.java
@@ -1,11 +1,73 @@
 package leetCode.medium;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
 
+  /**
+   * 8. String to Integer (atoi)
+   * 
+   * Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+   * 
+   * The algorithm for myAtoi(string s) is as follows:
+   * Read in and ignore any leading whitespace.
+   * Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. 
+   * This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+   * Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+   * Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. 
+   * Change the sign as necessary (from step 2).
+   * If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. 
+   * Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+   * Return the integer as the final result.
+   * 
+   * Note:
+   *  Only the space character ' ' is considered a whitespace character.
+   *  Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+   */
+  public int myAtoi(String s) {
+    int i=0;
+    long signal=1;
+    
+    //search for the signal
+    for(; i < s.length(); i++){
+      char c = s.charAt(i);
+      if (c == ' '){
+        continue;
+      } else if (c == '-') {
+        signal = -1;
+        i++;          
+      } else if (c == '+'){
+        signal = 1;
+        i++;          
+      } 
+      break;        
+    }
+    
+   // collect the digits
+    StringBuilder builder = new StringBuilder(s.length());
+    builder.append('0');
+    for(; i < s.length(); i++){
+      char c = s.charAt(i);
+      if(c >= '0' && c <= '9'){
+        builder.append(c);
+      } else {
+        break;
+      }        
+    }
+    
+    // treat values greater than integer
+    BigInteger bigValue = new BigInteger(builder.toString()).multiply(BigInteger.valueOf(signal));      
+    if (bigValue.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0){
+      return Integer.MAX_VALUE;
+    }else if (bigValue.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0){
+      return Integer.MIN_VALUE;
+    } else {
+      return bigValue.intValue();
+    }
+  }
 
   /**
    * 7. Reverse Integer
